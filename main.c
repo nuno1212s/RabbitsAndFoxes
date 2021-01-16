c0af7ec5abaf19ef27d949b1ab02d30ecc8b5171
nunogneto <nuno.neto.g@gmail.com>
2021-01-16 01:47:59 +0000

    made thread count use input

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv) {
     }
 
     if (!sequential) {
-        executeWithThreadCount(8, stdin, stdout);
+        executeWithThreadCount(threads, stdin, stdout);
     } else {
         executeSequentialThread(stdin, stdout);
     }
